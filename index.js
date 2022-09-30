a5527ee2e87e8ae27e76b2a97030b30e6bb014ee
BPGreenwood <53783989+B-P-Greenwood@users.noreply.github.com>
2022-09-30 12:01:07 +0100

    Created the random words/letters function

--- index.js
+++ index.js
@@ -2,12 +2,42 @@ function random(upper, lower) {
   //
   if (lower && upper) {
     return Math.floor(Math.random() * upper + lower);
-  } else if(upper) {
+  } else if (upper) {
     return Math.floor(Math.random() * (upper + 1));
   } else {
-    throw console.error("Expected 1 or 2 arguments, you did not supply any arguments");
+    throw console.error(
+      'Expected 1 or 2 arguments, you did not supply any arguments'
+    );
   }
 }
-export default random;
+//export default random;
 
-console.log(random());
+export function randomWords(str) {
+  let sentence = false;
+  let result = [];
+  // checks if sentence passed
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === ' ') sentence = true;
+  }
+  //if sentence passed reorder words
+  if (sentence) {
+    let source = str.split(' ');
+    while (source.length > 0) {
+      let index = Math.floor(Math.random() * source.length);
+      result.push(source[index]);
+      source.splice(index, 1);
+    }
+    return result.join(' ');
+    // else reorder letters in single word
+  } else {
+    let source = str.split('');
+    while (source.length > 0) {
+      let index = Math.floor(Math.random() * source.length);
+      result.push(source[index]);
+      source.splice(index, 1);
+    }
+    return result.join('');
+  }
+}
+
+console.log(randomWords('hellopeople'));
