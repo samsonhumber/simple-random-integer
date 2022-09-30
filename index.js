b244fad50409ca485c21464e3af8b5a8a0734c92
samsonhumber <printerfissh693@gmail.com>
2022-09-30 12:52:16 +0100

    gaussian function and tests but not yet passed

--- index.js
+++ index.js
@@ -8,6 +8,25 @@ function random(upper, lower) {
     throw console.error("Expected 1 or 2 arguments, you did not supply any arguments");
   }
 }
+
+export function gaussian(mean, std) {
+  // mean is centre of distribution - if not given, it is assumed 0
+  // std is standard deviation, how 'wide' the distribution is - if not given, it is assumed 1
+  // The approach here uses a Box-Muller transformation
+  const uniforms = [Math.random(), Math.random()];
+  const normals = [Math.sqrt(-2*Math.log(uniforms[0])) * Math.cos(2*Math.PI*uniforms[1]), Math.sqrt(-2*Math.log(uniforms[0])) * Math.sin(2*Math.PI*uniforms[1])];
+  //console.log(uniforms, normals);
+  if (mean && std && mean+std !== NaN) {
+    return normals[0]*std + mean
+  } else if(std && std !== NaN) {
+    return normals[0]*std
+  } else if (mean && mean !== NaN) {
+    return normals[0] + mean
+  } else {
+    return normals[0]
+  }  
+}
+
 export default random;
 
-console.log(random());
+//console.log(random());
---
+++ index.test.js
@@ -0,0 +1,52 @@
+import { test, expect, describe } from '@jest/globals';
+import { gaussian } from "./index.js";
+
+describe('Gaussian function tests', () => {
+    const sampleSize = 10000;
+    const tolerance = 0.05;
+    test('tests to see if the gaussian function distributes with a mean of 0 and standard deviation of 1 when no arguments passed', () => {
+        //ARRANGE
+        //const trialMean = 5;
+        //const trialStd = 2;
+        let gaussianSampleSum = 0;
+        let squareGaussianSampleSum = 0
+        for (let i=0; i<sampleSize; i++) {
+            const newSample = gaussian();
+            gaussianSampleSum += newSample;
+            squareGaussianSampleSum += newSample ** 2;
+        }
+        
+        //ACT 
+        const actualMean = gaussianSampleSum / sampleSize; 
+        const actualStd =  squareGaussianSampleSum / sampleSize; 
+        console.log("Mean and std", actualMean, actualStd)
+        //ASSERT
+        expect(actualMean).toBeGreaterThan(0 - tolerance);
+        expect(actualMean).toBeLessThan(0 + tolerance);
+        expect(actualStd).toBeGreaterThan(1 - tolerance);
+        expect(actualStd).toBeLessThan(1 + tolerance);
+    });
+
+    test('tests to see if the gaussian function distributes with a given mean and standard deviation', () => {
+        //ARRANGE
+        const trialMean = 5;
+        const trialStd = 2;
+        let gaussianSampleSum = 0;
+        let squareGaussianSampleSum = 0
+        for (let i=0; i<sampleSize; i++) {
+            const newSample = gaussian(trialMean, trialStd);
+            gaussianSampleSum += newSample;
+            squareGaussianSampleSum += Math.sqrt(newSample ** 2);
+        }
+        
+        //ACT 
+        const actualMean = gaussianSampleSum / sampleSize; 
+        const actualStd =  squareGaussianSampleSum / sampleSize; 
+        console.log("Mean and std", actualMean, actualStd)
+        //ASSERT
+        expect(actualMean).toBeGreaterThan(trialMean - tolerance);
+        expect(actualMean).toBeLessThan(trialMean + tolerance);
+        expect(actualStd).toBeGreaterThan(trialStd - tolerance);
+        expect(actualStd).toBeLessThan(trialStd + tolerance);
+    });
+});
