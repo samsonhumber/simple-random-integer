9bdceb365d391492f99ea3a0ab8909153585e26e
samsonhumber <printerfissh693@gmail.com>
2022-09-30 11:11:38 +0100

    some error handling for no arguments

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 function random(upper, lower) {
+  //
   if (lower && upper) {
     return Math.floor(Math.random() * upper + lower);
-  } else {
+  } else if(upper) {
     return Math.floor(Math.random() * (upper + 1));
+  } else {
+    throw console.error("Expected 1 or 2 arguments, you did not supply any arguments");
   }
 }
 export default random;
+
+console.log(random());
