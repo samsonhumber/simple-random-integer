fb7c2f0a3ad555ac96ad7a9a917f5d2922237626
samsonhumber <printerfissh693@gmail.com>
2022-09-30 14:26:16 +0100

    error handling improved and tested

--- index.js
+++ index.js
@@ -9,25 +9,34 @@ function random(upper, lower) {
   }
 }
 
-export function gaussian(mean, std) {
-  // mean is centre of distribution - if not given, it is assumed 0, std is standard deviation, how 'wide' the distribution is - if not given, it is assumed 1
-  // The approach here uses a Box-Muller transformation
+export function gaussian(mean=0, std=1) {
+  try{
+    if(typeof mean !== 'number' || typeof std !== 'number') {
+      console.log('error 1 thrown');
+      throw "Only accepts number inputs"
+    } else if (isNaN(mean) || isNaN(std)) {
+      console.log('error 2 thrown');
+      throw "Inputs cannot not be NaN"
+    } else {
+      // mean is centre of distribution - if not given, it is assumed 0, std is standard deviation, how 'wide' the distribution is - if not given, it is assumed 1
+      // The approach here uses a Box-Muller transformation
+      if (arguments.length>2){
+        console.log('WARNING: you may have entered more than 2 arguments, any additional arguments are ignored.')
+      }
+      const uniforms = [Math.random(), Math.random()];
+      const normals = [Math.sqrt(-2*Math.log(uniforms[0])) * Math.cos(2*Math.PI*uniforms[1])];
 
-  const uniforms = [Math.random(), Math.random()];
-  const normals = [Math.sqrt(-2*Math.log(uniforms[0])) * Math.cos(2*Math.PI*uniforms[1])];
+      // The orginal formula says that the sin version is independent but on same distribution
+      //const normals = [Math.sqrt(-2*Math.log(uniforms[0])) * Math.cos(2*Math.PI*uniforms[1]), Math.sqrt(-2*Math.log(uniforms[0])) * Math.sin(2*Math.PI*uniforms[1])];
 
-  // The orginal formula says that the sin version is independent but on same distribution
-  //const normals = [Math.sqrt(-2*Math.log(uniforms[0])) * Math.cos(2*Math.PI*uniforms[1]), Math.sqrt(-2*Math.log(uniforms[0])) * Math.sin(2*Math.PI*uniforms[1])];
+      return normals[0]*std + mean
+    }
+  } catch(err) {
+    console.error('Invalid input arguments. Mean is', mean, ', standard deviation is', std);
+    throw(err);
+  }
   
-  if (mean && std && mean+std !== NaN) {
-    return normals[0]*std + mean
-  } else if(std && std !== NaN) {
-    return normals[0]*std
-  } else if (mean && mean !== NaN) {
-    return normals[0] + mean
-  } else {
-    return normals[0]
-  }  
+  
 }
 
 export default random;
--- index.test.js
+++ index.test.js
@@ -2,8 +2,8 @@ import { test, expect, describe } from '@jest/globals';
 import { gaussian } from "./index.js";
 
 describe('Gaussian function tests', () => {
-    const sampleSize = 10000;
-    const tolerance = 0.05;
+    const sampleSize = 100000;
+    const tolerance = 0.01;
     test('tests to see if the gaussian function distributes with a mean of 0 and standard deviation of 1 when no arguments passed', () => {
         //ARRANGE
         //const trialMean = 5;
@@ -30,7 +30,7 @@ describe('Gaussian function tests', () => {
     test('tests to see if the gaussian function distributes with a given mean and standard deviation', () => {
         //ARRANGE
         const trialMean = 5;
-        const trialStd = 2;
+        const trialStd = 100;
         let gaussianSampleSum = 0;
         let squareGaussianSampleSum = 0
         for (let i=0; i<sampleSize; i++) {
@@ -44,9 +44,64 @@ describe('Gaussian function tests', () => {
         const actualStd =  Math.sqrt(squareGaussianSampleSum / (sampleSize-1)); 
         console.log("Mean and std", actualMean, actualStd)
         //ASSERT
+        expect(actualMean).toBeGreaterThan(trialMean - tolerance*trialStd);
+        expect(actualMean).toBeLessThan(trialMean + tolerance*trialStd);
+        expect(actualStd).toBeGreaterThan(trialStd - tolerance*trialStd);
+        expect(actualStd).toBeLessThan(trialStd + tolerance*trialStd);
+    });
+
+    test('tests to see if the gaussian function distributes with a given mean and standard deviation', () => {
+        //ARRANGE
+        const trialMean = 5;
+        const trialStd = 100;
+        let gaussianSampleSum = 0;
+        let squareGaussianSampleSum = 0
+        for (let i=0; i<sampleSize; i++) {
+            const newSample = gaussian(trialMean, trialStd);
+            gaussianSampleSum += newSample;
+            squareGaussianSampleSum += (newSample-trialMean) ** 2;
+        }
+        
+        //ACT 
+        const actualMean = gaussianSampleSum / sampleSize; 
+        const actualStd =  Math.sqrt(squareGaussianSampleSum / (sampleSize-1)); 
+        console.log("Mean and std", actualMean, actualStd)
+        //ASSERT
+        expect(actualMean).toBeGreaterThan(trialMean - tolerance*trialStd);
+        expect(actualMean).toBeLessThan(trialMean + tolerance*trialStd);
+        expect(actualStd).toBeGreaterThan(trialStd - tolerance*trialStd);
+        expect(actualStd).toBeLessThan(trialStd + tolerance*trialStd);
+    });
+
+    test('tests to see if the gaussian function distributes with a given mean but no given standard deviation', () => {
+        //ARRANGE
+        const trialMean = 5;
+        //const trialStd = 100;
+        let gaussianSampleSum = 0;
+        let squareGaussianSampleSum = 0
+        for (let i=0; i<sampleSize; i++) {
+            const newSample = gaussian(trialMean);
+            gaussianSampleSum += newSample;
+            squareGaussianSampleSum += (newSample-trialMean) ** 2;
+        }
+        
+        //ACT 
+        const actualMean = gaussianSampleSum / sampleSize; 
+        const actualStd =  Math.sqrt(squareGaussianSampleSum / (sampleSize-1)); 
+        console.log("Mean and std", actualMean, actualStd);
+        //ASSERT
         expect(actualMean).toBeGreaterThan(trialMean - tolerance);
         expect(actualMean).toBeLessThan(trialMean + tolerance);
-        expect(actualStd).toBeGreaterThan(trialStd - tolerance);
-        expect(actualStd).toBeLessThan(trialStd + tolerance);
+        expect(actualStd).toBeGreaterThan(1 - tolerance);
+        expect(actualStd).toBeLessThan(1 + tolerance);
+    });
+
+    test('tests to see if non-number types throw an error', () => {
+        //ARRANGE
+        const trialMean = 5;
+        const trialStd = '100';
+        //ACT 
+        //ASSERT
+        expect(() => gaussian(trialMean, trialStd)).toThrow('Only accepts number inputs');
     });
 });
